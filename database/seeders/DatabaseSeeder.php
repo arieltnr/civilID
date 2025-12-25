414923b99588b6b5ab1800c3623516d0119c7c8b
Ariel T.N <arielteguhn.inovamedika@gmail.com>
2025-12-25 23:08:17 +0700

    Config

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,20 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        /*
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        */
+
+        $super_admin = User::where('email', 'sysadmin123@gmail.com')->first();
+        if (empty($super_admin)) {
+            User::create([
+                'name' => 'Sysadmin',
+                'email' => 'sysadmin123@gmail.com',
+                'password' => Hash::make('Abcd-4321'),
+            ]);
+        }
     }
 }
